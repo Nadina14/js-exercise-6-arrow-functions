ad9de4688f478eb9cd0376bf7a9ef9024eaa1803
Nadina <sheriefnadina@gmail.com>
2023-11-06 18:39:09 +0100

    feat/ esercizio-3

--- esercizio-3/main.js
+++ esercizio-3/main.js
@@ -1,2 +1,9 @@
 /* Crea una semplice pagina HTML con un elemento div. 
 Usa JavaScript per cambiare l'innerHTML e l'innerText del div. */
+
+window.addEventListener('load', () => {
+    const elemento = document.getElementById('div-1');
+    elemento.innerHTML += `<p>Ciao</p>`;
+    const elemento2 = document.getElementById('div-2');
+    elemento2.innerText = 'Addio'; 
+})
