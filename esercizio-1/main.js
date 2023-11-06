60cb965db8b7edf92e9563f3755adaa0dd6146a9
Nadina <sheriefnadina@gmail.com>
2023-11-06 18:09:42 +0100

    feat/ esercizio-1

--- esercizio-1/main.js
+++ esercizio-1/main.js
@@ -1,1 +1,11 @@
 /* Crea una arrow functio che accetta due numeri come parametri e restituisce la loro sottrazione. */
+
+const num1Utente = Number(prompt('Inserisci un numero'));
+const num2Utente = Number(prompt('Inserisci un numero maggiore di quello inserito prima'));
+
+window.addEventListener('load', sottrazione = (num1, num2) => {
+    return num2 - num1;
+})
+
+const risultato = sottrazione(num1Utente, num2Utente);
+console.log(risultato)
