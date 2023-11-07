86021f4eac14d9a6f08455186fc948246af98e8e
Nadina <sheriefnadina@gmail.com>
2023-11-07 10:46:11 +0100

    correzione es 3

--- esercizio-3/main.js
+++ esercizio-3/main.js
@@ -3,7 +3,6 @@ Usa JavaScript per cambiare l'innerHTML e l'innerText del div. */
 
 window.addEventListener('load', () => {
     const elemento = document.getElementById('div-1');
+    elemento.innerText = 'Addio'; 
     elemento.innerHTML += `<p>Ciao</p>`;
-    const elemento2 = document.getElementById('div-2');
-    elemento2.innerText = 'Addio'; 
 })
